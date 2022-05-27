9b9076a433fde79e81ec45284a4cab15caedb3fa
Samuel Guilherme <samuel.guilherme.l@hotmail.com>
2022-05-27 00:36:15 -0300

    feat: add Entity User

---
+++ src/main/java/com/example/Application/Model/User.java
@@ -0,0 +1,15 @@
+package com.example.Application.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.Auto);
+    private long id;
+    private String name;
+}
