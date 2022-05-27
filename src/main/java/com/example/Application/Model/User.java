d0180cb076f7fdb269f5e76a4f2b6955efd1b496
Samuel Guilherme <samuel.guilherme.l@hotmail.com>
2022-05-27 02:08:57 -0300

    create class user

--- src/main/java/com/example/Application/Model/User.java
+++ src/main/java/com/example/Application/Model/User.java
@@ -2,6 +2,7 @@ package com.example.Application.Model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -9,7 +10,8 @@ import javax.persistence.Table;
 @Table(name = "user")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.Auto);
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    
     private String name;
 }
